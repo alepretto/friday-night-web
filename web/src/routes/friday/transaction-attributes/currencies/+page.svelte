c61809ab103def8221e4629f28116fb26f57fe0c
Alexandre Pretto <ale.pretto@gmail.com>
2026-02-21 10:06:40 -0300

    Adicionando modal new Currency

--- web/src/routes/friday/transaction-attributes/currencies/+page.svelte
+++ web/src/routes/friday/transaction-attributes/currencies/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { SlidersHorizontal } from 'lucide-svelte';
+	import Modal from '$lib/components/ui/Modal.svelte';
+
 	import Table from '$lib/components/ui/Table.svelte';
 	import type { Column } from '$lib/components/ui/Table.svelte';
-	type CurrencyType = 'Fiat' | 'Cripto';
+	type CurrencyType = 'fiat' | 'crypto';
 
 	interface Currency {
 		id: number;
@@ -19,17 +21,17 @@
 		{ key: 'actions', label: 'Actions', align: 'center' }
 	];
 
-	const currencies: Currency[] = [
-		{ id: 1, label: 'Brazilian Real', symbol: 'R$', type: 'Fiat' },
-		{ id: 2, label: 'United States Dollar', symbol: '$', type: 'Fiat' },
-		{ id: 3, label: 'Bitcoin', symbol: 'BTC', type: 'Cripto' },
-		{ id: 4, label: 'Tether', symbol: 'USDT', type: 'Cripto' },
-		{ id: 5, label: 'Ethereum', symbol: 'ETH', type: 'Cripto' }
-	];
+	const currencies: Currency[] = $state([
+		{ id: 1, label: 'Brazilian Real', symbol: 'R$', type: 'fiat' },
+		{ id: 2, label: 'United States Dollar', symbol: '$', type: 'fiat' },
+		{ id: 3, label: 'Bitcoin', symbol: 'BTC', type: 'crypto' },
+		{ id: 4, label: 'Tether', symbol: 'USDT', type: 'crypto' },
+		{ id: 5, label: 'Ethereum', symbol: 'ETH', type: 'crypto' }
+	]);
 
 	const typeBadge = {
-		Fiat: 'bg-friday-blue/50 border-blue-600 text-white',
-		Cripto: 'bg-failed/60 border border-red-900 text-red-200'
+		fiat: 'bg-friday-blue/50 border-blue-600 text-white',
+		crypto: 'bg-failed/60 border border-red-900 text-red-200'
 	};
 	const totalPages = 68;
 	let currentPage = $state(1);
@@ -46,11 +48,83 @@
 	}
 
 	const visiblePages = $derived(getVisiblePages(currentPage, totalPages));
+
+	// Campos Modais
+	let nextId = $state(6);
+	let openModal = $state(false);
+
+	let label = $state('');
+	let symbol = $state('');
+	let type: CurrencyType | '' = $state('');
+
+	function handleSave() {
+		if (!label.trim() || !symbol.trim()) return;
+		if (!type) return;
+
+		symbol = symbol.trim();
+		label = label.trim();
+		currencies.push({
+			id: nextId,
+			label,
+			symbol,
+			type
+		});
+		label = '';
+		symbol = '';
+		type = '';
+		openModal = false;
+	}
 </script>
+
+<Modal
+	title="New Currency"
+	open={openModal}
+	onclose={() => (openModal = false)}
+	onsave={handleSave}
+>
+	{#snippet body()}
+		<div class="flex flex-col gap-4">
+			<div class="flex w-full flex-col gap-3">
+				<label class="text-bold pl-4 text-xl" for="label">Descrição</label>
+				<input
+					bind:value={label}
+					class="w-full rounded-xl bg-white/10 px-4 py-2 text-white outline-none focus:ring-2 focus:ring-white/20"
+					type="text"
+					id="label"
+				/>
+			</div>
+			<div class="grid grid-cols-2 gap-4">
+				<div class="flex w-full flex-col gap-3">
+					<label class="text-bold pl-4 text-xl" for="symbol">Symbol</label>
+					<input
+						bind:value={symbol}
+						class="w-full rounded-xl bg-white/10 px-4 py-2 text-white outline-none focus:ring-2 focus:ring-white/20"
+						type="text"
+						id="symbol"
+					/>
+				</div>
+				<div class="flex w-full flex-col gap-3">
+					<label class="text-bold pl-4 text-xl" for="label">Type</label>
+					<select
+						name="currency-type"
+						id="currency-type"
+						bind:value={type}
+						class="w-full rounded-xl bg-white/10 px-4 py-2 text-white outline-none focus:ring-2 focus:ring-white/20"
+					>
+						<option class="px-4 py-2 text-black" value="">Selecione...</option>
+						<option class="px-4 py-2 text-black" value="fiat">Fiat</option>
+						<option class="px-4 py-2 text-black" value="crypto">Crypto</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	{/snippet}
+</Modal>
 
 <section>
 	<div class="flex items-center justify-end p-10">
 		<button
+			onclick={() => (openModal = true)}
 			class="cursor-pointer rounded-3xl border border-success/60 bg-success/40 px-10 py-3 transition-colors hover:bg-success"
 		>
 			New Currency
