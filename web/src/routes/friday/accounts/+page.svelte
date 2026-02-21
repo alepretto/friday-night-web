835d467ca741a75100a3a6ecaa07d879fb7eb0a4
Alexandre Pretto <ale.pretto@gmail.com>
2026-02-21 17:31:41 -0300

    Refatorando Account cards e listando todos os tipos. Closes #16

--- web/src/routes/friday/accounts/+page.svelte
+++ web/src/routes/friday/accounts/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AccountCard from './AccountCard.svelte';
 	type AccountType = 'bank' | 'investment' | 'cash' | 'benefict';
 
 	interface Account {
@@ -6,12 +7,39 @@
 		institution: string;
 		active: boolean;
 		type: AccountType;
+		logoPath: string;
 		subtype?: string | null;
 	}
 	const accounts: Account[] = [
-		{ id: 1, institution: 'Santander', active: true, type: 'bank' },
-		{ id: 2, institution: 'BTG Pactual', active: true, type: 'investment' },
-		{ id: 3, institution: 'Pluxee', active: true, type: 'benefict', subtype: 'Vale Refeição' }
+		{
+			id: 1,
+			institution: 'Santander',
+			active: true,
+			type: 'bank',
+			logoPath: '/logo-santander 1.png'
+		},
+		{
+			id: 2,
+			institution: 'BTG Pactual',
+			active: true,
+			type: 'investment',
+			logoPath: '/logo-btg.png'
+		},
+		{
+			id: 3,
+			institution: 'Pluxee',
+			active: true,
+			type: 'benefict',
+			subtype: 'Vale Refeição',
+			logoPath: '/logo-pluxee.png'
+		},
+		{
+			id: 3,
+			institution: 'Picnic',
+			active: true,
+			type: 'cash',
+			logoPath: '/logo-picnic.png'
+		}
 	];
 </script>
 
@@ -30,51 +58,58 @@
 			</button>
 		</div>
 
-		{#if accounts.filter((a) => a.type == 'bank').length}
-			<div class="my-3 border-b border-secondary/50 pb-3">
-				<span class=" text-4xl font-bold">Bank</span>
-			</div>
-			<div class="grid grid-cols-3 gap-4">
-				{#each accounts.filter((a) => a.type == 'bank') as account (account.id)}
-					<div class="flex flex-col rounded-2xl border border-white/20 bg-secondary/30 px-4 py-5">
-						<div class="flex items-center border-b border-b-white/20">
-							<div class="p-5">
-								<img src="/logo-santander 1.png" alt="" class="h-15 w-15" />
-							</div>
-							<div class="flex flex-col">
-								<span class="text-3xl font-bold">{account.institution}</span>
-								<span class="text-xl">Conta Corrente</span>
-							</div>
-						</div>
-						<div class="flex justify-between py-6">
-							<div class="flex flex-col gap-3 pl-3">
-								<div>
-									<span class="text-2xl font-bold">R$ 5.538,23</span>
-								</div>
-								<div class="flex items-center gap-5">
-									<div class="h-5 w-5 rounded-full bg-success"></div>
-									<span class="text-xl">Status</span>
-								</div>
-							</div>
-							<div class="flex items-end">
-								<div
-									class="grid grid-cols-3 gap-2 rounded-xl border-3 border-secondary/30 bg-tertiary"
-								>
-									<button class="cursor-pointer px-5 py-2 text-center hover:bg-secondary">
-										Ver
-									</button>
-									<button class="cursor-pointer px-5 py-2 text-center hover:bg-secondary">
-										Editar
-									</button>
-									<button class="cursor-pointer px-5 py-2 text-center hover:bg-secondary"
-										>Arquivar</button
-									>
-								</div>
-							</div>
-						</div>
+		<div class="flex flex-col gap-4">
+			<div>
+				{#if accounts.filter((a) => a.type == 'bank').length}
+					<div class="my-3 border-b border-secondary/50 pb-3">
+						<span class=" text-4xl font-bold">Bank</span>
 					</div>
-				{/each}
+					<div class="grid grid-cols-3 gap-4">
+						{#each accounts.filter((a) => a.type == 'bank') as account (account.id)}
+							<AccountCard {account} />
+						{/each}
+					</div>
+				{/if}
 			</div>
-		{/if}
+
+			<div>
+				{#if accounts.filter((a) => a.type == 'investment').length}
+					<div class="my-3 border-b border-secondary/50 py-3">
+						<span class=" text-4xl font-bold">Investimentos</span>
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						{#each accounts.filter((a) => a.type == 'investment') as account (account.id)}
+							<AccountCard {account} />
+						{/each}
+					</div>
+				{/if}
+			</div>
+
+			<div>
+				{#if accounts.filter((a) => a.type == 'cash').length}
+					<div class="my-3 border-b border-secondary/50 py-3">
+						<span class=" text-4xl font-bold">Cash</span>
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						{#each accounts.filter((a) => a.type == 'cash') as account (account.id)}
+							<AccountCard {account} />
+						{/each}
+					</div>
+				{/if}
+			</div>
+
+			<div>
+				{#if accounts.filter((a) => a.type == 'benefict').length}
+					<div class="my-3 border-b border-secondary/50 py-3">
+						<span class=" text-4xl font-bold">Benefícios</span>
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						{#each accounts.filter((a) => a.type == 'benefict') as account (account.id)}
+							<AccountCard {account} />
+						{/each}
+					</div>
+				{/if}
+			</div>
+		</div>
 	</section>
 </main>
